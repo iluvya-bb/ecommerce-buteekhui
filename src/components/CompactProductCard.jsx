8a7ff41bc2515b4f35fcde94036985d8e732b3f8
Luvsannyam Bayarsaikhan <me@luvya.dev>
2025-08-21 06:35:16 +0800

    ADDED : settings controller

--- src/components/CompactProductCard.jsx
+++ src/components/CompactProductCard.jsx
@@ -2,38 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CompactProductCard = ({ product }) => {
-  const imageUrl = product.featuredImage
-    ? `${import.meta.env.VITE_API_URL}/${product.featuredImage}`
-    : product.images && product.images[0]
-      ? `${import.meta.env.VITE_API_URL}/${product.images[0].url}`
-      : "https://via.placeholder.com/150";
-  const categoryText = product.categories
-    ? product.categories.map((c) => c.name).join(", ")
-    : "";
+	const imageUrl = product.featuredImage
+		? `${import.meta.env.VITE_API_URL}/${product.featuredImage}`
+		: product.images && product.images[0]
+			? `${import.meta.env.VITE_API_URL}/${product.images[0].url}`
+			: "https://via.placeholder.com/150";
+	const categoryText = product.categories
+		? product.categories.map((c) => c.name).join(", ")
+		: "";
 
-  return (
-    <Link to={`/product/${product.id}`}>
-      <div className="bg-white rounded-lg shadow-md overflow-hidden">
-        <img
-          src={imageUrl}
-          alt={product.name}
-          className="w-full h-32 object-contain"
-        />
-        <div className="p-2">
-          <h3 className="text-sm font-semibold h-10 truncate">
-            {product.name}
-          </h3>
-          <p className="text-xs text-gray-500 h-6">{categoryText}</p>
-          <div className="mt-1">
-            <p className="text-sm font-bold">₮{product.price}</p>
-            <button className="bg-blue-500 text-white px-2 py-1 rounded-lg text-xs w-full mt-2">
-              Сагсанд хийх
-            </button>
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link to={`/product/${product.id}`}>
+			<div className="bg-white rounded-lg shadow-md overflow-hidden">
+				<img
+					src={imageUrl}
+					alt={product.name}
+					className="w-full h-32 object-contain"
+				/>
+				<div className="p-2">
+					<h3 className="text-sm font-semibold h-10 truncate">
+						{product.name}
+					</h3>
+					<p className="text-xs text-gray-500 h-6">{categoryText}</p>
+					<div className="mt-1">
+						<p className="text-sm font-bold">₮{product.price}</p>
+						<button className="bg-blue-500 text-white px-2 py-1 rounded-lg text-xs w-full mt-2">
+							Сагсанд хийх
+						</button>
+					</div>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default CompactProductCard;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,48 +3,48 @@ import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 
 const ProductCard = ({ product }) => {
-  const { addToCart } = useContext(CartContext);
-  const imageUrl = product.featuredImage
-    ? `${import.meta.env.VITE_API_URL}/${product.featuredImage}`
-    : product.images && product.images[0]
-      ? `${import.meta.env.VITE_API_URL}/${product.images[0].url}`
-      : "https://via.placeholder.com/150";
-  const categoryText = product.categories
-    ? product.categories.map((c) => c.name).join(", ")
-    : "";
+	const { addToCart } = useContext(CartContext);
+	const imageUrl = product.featuredImage
+		? `${import.meta.env.VITE_API_URL}/${product.featuredImage}`
+		: product.images && product.images[0]
+			? `${import.meta.env.VITE_API_URL}/${product.images[0].url}`
+			: "https://via.placeholder.com/150";
+	const categoryText = product.categories
+		? product.categories.map((c) => c.name).join(", ")
+		: "";
 
-  const handleAddToCart = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    addToCart(product, 1);
-  };
+	const handleAddToCart = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		addToCart(product, 1);
+	};
 
-  return (
-    <Link to={`/product/${product.id}`}>
-      <div className="bg-white rounded-lg shadow-md overflow-hidden">
-        <img
-          src={imageUrl}
-          alt={product.name}
-          className="w-full h-48 object-contain"
-        />
-        <div className="p-4">
-          <h3 className="text-base font-semibold text-black">{product.name}</h3>
-          <p className="text-sm text-gray-500">{categoryText}</p>
-          <div className="mt-2 flex justify-between items-center text-black">
-            <p className="text-lg font-bold" text-black>
-              ₮{product.price}
-            </p>
-            <button
-              onClick={handleAddToCart}
-              className="bg-blue-500 text- px-3 py-1 rounded-lg text-sm"
-            >
-              Сагсанд хийх
-            </button>
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link to={`/product/${product.id}`}>
+			<div className="bg-white rounded-lg shadow-md overflow-hidden">
+				<img
+					src={imageUrl}
+					alt={product.name}
+					className="w-full h-48 object-contain"
+				/>
+				<div className="p-4">
+					<h3 className="text-base font-semibold text-black">{product.name}</h3>
+					<p className="text-sm text-gray-500">{categoryText}</p>
+					<div className="mt-2 flex justify-between items-center text-black">
+						<p className="text-lg font-bold" text-black>
+							₮{product.price}
+						</p>
+						<button
+							onClick={handleAddToCart}
+							className="bg-blue-500 text-white px-3 py-1 rounded-lg text-sm"
+						>
+							Сагсанд хийх
+						</button>
+					</div>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default ProductCard;
